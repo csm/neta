c40a1e9841c1b6bcc6a406f4d2f51adce6f5055f
casey.s.marshall <casey.s.marshall@e1f095a7-2925-0410-8641-1396461ec383>
2007-01-02 19:28:49 +0000

    Update copyright headers. Add 'COPYING'.

--- NANetworkDevice.h
+++ NANetworkDevice.h
@@ -1,10 +1,23 @@
-//
-//  NANetworkDevice.h
-//  Network Analyzer
-//
-//  Created by C. Scott Marshall on 12/17/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/* NANetworkDevice.h -- a network interface.
+   Copyright (C) 2006, 2007  Casey Marshall <casey.s.marshall@gmail.com>
+
+This file is a part of Network Analyzer.
+
+Network Analyzer is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301  USA  */
+
 
 #import <Cocoa/Cocoa.h>
 #import "NAInternetAddress.h"
--- NAProtocols.h
+++ NAProtocols.h
@@ -1,11 +1,23 @@
-/*
- *  NAProtocols.h
- *  Network Analyzer
- *
- *  Created by C. Scott Marshall on 12/21/06.
- *  Copyright 2006 __MyCompanyName__. All rights reserved.
- *
- */
+/* NAProtocols.h -- network protocol structures.
+   Copyright (C) 2006, 2007  Casey Marshall <casey.s.marshall@gmail.com>
+
+This file is a part of Network Analyzer.
+
+Network Analyzer is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301  USA  */
+
 
 #include <stdint.h>
 #define ETHER_ADDR_LEN 6
